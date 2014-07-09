82f765768dccc1cc45b81d01af471e15af104679
Peter Berry (CSNB419) <pwcberry@gmail.com>
2014-07-09 22:56:00 +1000

    Added a couple more validation directives. Will change testing suite to Protractor to allow for better interaction testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,40 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-    grunt.initConfig({
-        jshint: {
-            files: ['./src/js/**/*.js', './test/**/*Spec.js']
-        },
-        copy: {
-            lib: {
-                files:[ {
-                    expand: true,
-                    flatten: true,
-                    src: ['./lib/angular/angular.js'],
-                    dest: './demo/public/javascripts'
-                }]
-            },
-            demo: {
-                files: [{
-                    expand: false,
-                    flatten: true,
-                    src: ['./src/js/*.js'],
-                    dest: './demo/public/javascripts/validation.js'
-                }]
-            }
-        },
-        karma: {
-            unit: {
-                configFile:'karma.conf.js',
-                singleRun: true
-            }
-        }
-    });
+	grunt.initConfig({
+		jshint: {
+			files: ['./src/js/**/*.js']
+		},
+		copy: {
+			lib: {
+				files: [{
+					expand: true,
+					flatten: true,
+					src: ['./lib/angular/angular.js'],
+					dest: './demo/public/javascripts'
+				}]
+			},
+			demo: {
+				files: [{
+					expand: false,
+					flatten: true,
+					src: ['./src/js/*.js'],
+					dest: './demo/public/javascripts/validation.js'
+				}]
+			}
+		},
+		karma: {
+			unit: {
+				configFile: 'karma.conf.js'
+			}
+		}
+	});
 
-//    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-karma');
+	//    grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-karma');
 
-    grunt.registerTask('default', ['jshint', 'copy']);
+	grunt.registerTask('test', ['jshint', 'karma']);
+	grunt.registerTask('default', ['jshint', 'copy']);
 
 };
--- src/js/ValidationDirectives.js
+++ src/js/ValidationDirectives.js
@@ -1,57 +1,119 @@
-(function (angular) {
+(function(angular) {
 
-    /**
-     * Custom directives to handle form validation.
-     *
-     * The 'Validator' function will be moved to
-     * become a service to allow other directives to
-     * use the same code.
-     */
-    angular.module('Validation', [])
-        .directive('valRequired', function () {
-            var Validator = function (element, container, validate) {
-                return function () {
-                    var isValid = validate(element[0]);
-                    if (!isValid) {
-                        container.addClass('has-validation-error');
-                    } else {
-                        container.removeClass('has-validation-error');
-                    }
+	/**
+	 * Private function factory to create validators.
+	 *
+	 * @param {Object} the form element that is the target of validation
+	 * @param {Object} the form element's container node
+	 * @param {Function} the validation function to invoke
+	 */
+	var Validator = function(element, container, validate) {
+		return function() {
+			var isValid = !element.hasClass('has-error') && validate(element[0]);
 
-                    return isValid;
-                };
-            };
+			if (!isValid) {
+				container.addClass('has-validation-error');
+				element.addClass('has-error');
+			} else {
+				container.removeClass('has-validation-error');
+				element.removeClass('has-error');
+			}
 
-            return {
-                restrict: 'A',
-                link: function (scope, element, attr) {
-                    var err, message;
+			return isValid;
+		};
+	};
 
-                    if (!attr.valRequired) {
-                        message = 'This field is mandatory';
-                    } else {
-                        message = attr.valRequired;
-                    }
-                    err = angular.element('<span class="error-message">' + message + '</span>');
-                    element.parent().append(err);
-                },
-                controller: function ($scope, $element) {
-                    var validate = Validator($element, $element.parent(), function (el) {
-                        var val;
+	/**
+	 * Custom directives to handle form validation.
+	 */
+	angular.module('Validation', [])
+		.directive('valRequired', function() {
+			return {
+				restrict: 'A',
+				link: function($scope, $element, $attr) {
+					var message = (!$attr.valRequired ? 'This field is mandatory' : $attr.valRequired),
+						err = angular.element('<span class="error-message">' + message + '</span>');
 
-                        if (el.type == 'checkbox') {
-                            return el.checked;
-                        } else if (el.type == 'file') {
-                            return el.files.length;
-                        }
+					$element.parent().append(err);
+				},
+				controller: function($scope, $element) {
+					var validate = Validator($element, $element.parent(), function(el) {
+						var val;
 
-                        val = el.value.trim();
-                        return val.length > 0;
-                    });
+						if (el.type == 'checkbox') {
+							return el.checked;
+						} else if (el.type == 'file') {
+							return el.files.length;
+						}
+						val = el.value.trim();
+                        console.log('valRequired validator');
+                        console.log('val = "' + val + '"');
+						return val.length > 0;
+					});
+					$element.on('blur', validate);
+				}
+			};
+		}).directive('valCode', function() {
+			return {
+				restrict: 'A',
+				link: function($scope, $element, $attr) {
+					var message = (!$attr.valCode ? 'Invalid' : $attr.valCode),
+						err = angular.element('<span class="error-message">' + message + '</span>');
 
-                    $element.on('blur', validate);
-                }
-            };
-        });
+					$element.parent().append(err);
+					$scope.length = parseInt($attr.valLength, 10);
+				},
+				controller: function($scope, $element) {
+					var validate = Validator($element, $element.parent(), function(el) {
+						var val = el.value.trim(),
+							code = parseInt(val, 10);
+						return $scope.length === val.length && !isNaN(code);
+					});
+					$element.on('blur', validate);
+				}
+			};
+		}).directive('valMobile', function() {
+			return {
+				restrict: 'A',
+				link: function($scope, $element, $attr) {
+					var message = (!$attr.valMobile ? 'Invalid' : $attr.valMobile),
+						err = angular.element('<span class="error-message">' + message + '</span>');
+					$element.parent().append(err);
+				},
+				controller: function($scope, $element) {
+					var validate = Validator($element, $element.parent(), function(el) {
+						var reMobile = /^(04\d\d)(\d{6})$/,
+							val = el.value.replace(/\s/g, '');
+						return val.length === 10 && reMobile.test(val);
+					});
+					$element.on('blur', validate);
+				}
+			};
+		}).directive('valTelephone', function() {
+			return {
+				restrict: 'A',
+				link: function($scope, $element, $attr) {
+					var message = (!$attr.valTelephone ? 'Invalid' : $attr.valTelephone),
+						err = angular.element('<span class="error-message">' + message + '</span>');
+					$element.parent().append(err);
+				},
+				controller: function($scope, $element) {
+					var validate = Validator($element, $element.parent(), function(el) {
+						var reFixed = /^(0[2378]|13)([02-9]\d{7})$/,
+							reMobile = /^(04\d\d)(\d{6})$/,
+							val = el.value.replace(/\s/g, ''),
+							phone = (reFixed.test(val) && RegExp.$2);
+
+						if (phone && phone.length === 8) {
+							return true;
+						} else {
+							phone = (reMobile.test(val) && RegExp.$2);
+							return phone && (phone.length === 6);
+						}
+					});
+					$element.on('blur', validate);
+				}
+			};
+		});
 
 }(angular));
